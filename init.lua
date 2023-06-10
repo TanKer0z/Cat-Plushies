d38a35e45c518702d28e965ae4dfe1d10d01b603
Atlante <108891261+AtlanteFr@users.noreply.github.com>
2023-06-10 12:55:12 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,20 @@
+    minetest.register_node("cat_plushies:chat", {
+        description = "?",
+    drawtype = "mesh",
+        mesh = "chat.obj",
+        tiles = {"chat.png"},
+        paramtype2 = "facedir",
+        selection_box = {
+            type= "fixed",
+            fixed = {-0.3, -0.46, -0.4, 0.3, 0.3, 0.3}
+        },
+        collision_box = {
+            type = "fixed",
+            fixed = {-0.3, -0.46, -0.4, 0.3, 0.3, 0.3}
+        },
+        inventory_image = "Il faut mettre une texture :p.png",
+        is_ground_content = false,
+        groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
+        flammable = 3, wool = 1}
+    })
+
